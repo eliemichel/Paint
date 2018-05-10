4434d641711b0bdbcff9e3e7dce7bfcee0270449
Elie Michel <elie.michel@ens.fr>
2018-05-10 16:04:49 +0200

    Add nanovg

--- src/paint/main.cpp
+++ src/paint/main.cpp
@@ -1,6 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <nanovg.h>
+#define NANOVG_GLES3_IMPLEMENTATION
+#include <nanovg_gl.h>
+
 #include <iostream>
 
 // Function prototypes
@@ -12,15 +16,15 @@ const GLuint WIDTH = 800, HEIGHT = 600;
 // The MAIN function, from here we start the application and run the game loop
 int main()
 {
-	std::cout << "Starting GLFW context, OpenGL ES 3.2" << std::endl;
+	std::cout << "Starting GLFW context, OpenGL ES 3.0" << std::endl;
 	// Init GLFW
 	glfwInit();
 	// Set all the required options for GLFW
 	glfwWindowHint(GLFW_OPENGL_API, GLFW_OPENGL_ES_API);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	// Create a GLFWwindow object that we can use for GLFW's functions
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Paint", NULL, NULL);
@@ -41,23 +45,58 @@ int main()
 		return -1;
 	}
 
-	// Define the viewport dimensions
-	glViewport(0, 0, WIDTH, HEIGHT);
+	// Init NanoVG
+	glfwMakeContextCurrent(window);
+	std::cout << "Starting NanoVG" << std::endl;
+	struct NVGcontext* vg = nvgCreateGLES3(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG); // TODO: try w/o NVG_STENCIL_STROKES
+	if (vg == NULL) {
+		std::cout << "Failed to initialize NanoVG context" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
 
 	// Game loop
 	while (!glfwWindowShouldClose(window))
 	{
-		// Check if any events have been activated (key pressed, mouse moved etc.) and call corresponding response functions
-		glfwPollEvents();
+		int winWidth, winHeight;
+		int fbWidth, fbHeight;
+		float pxRatio;
+
+		glfwGetWindowSize(window, &winWidth, &winHeight);
+		glfwGetFramebufferSize(window, &fbWidth, &fbHeight);
+		// Calculate pixel ration for hi-dpi devices.
+		pxRatio = (float)fbWidth / (float)winWidth;
+
+		glViewport(0, 0, fbWidth, fbHeight);
 
 		// Render
 		// Clear the colorbuffer
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glEnable(GL_CULL_FACE);
+		glDisable(GL_DEPTH_TEST);
+
+		nvgBeginFrame(vg, winWidth, winHeight, pxRatio);
+
+		nvgBeginPath(vg);
+		nvgRect(vg, 100, 100, 120, 30);
+		nvgFillColor(vg, nvgRGBA(255, 192, 0, 255));
+		nvgFill(vg);
+
+		nvgEndFrame(vg);
 
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
+
+		// Check if any events have been activated (key pressed, mouse moved etc.) and call corresponding response functions
+		glfwPollEvents();
 	}
+
+	// Destroy NanoVG ctxw
+	nvgDeleteGLES3(vg);
 
 	// Terminates GLFW, clearing any resources allocated by GLFW.
 	glfwTerminate();
